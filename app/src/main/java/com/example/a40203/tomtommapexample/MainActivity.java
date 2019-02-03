e0c474bc181c3f90837462f5d6fc89daf073c086
Dylan Tyrie-Dron <dman9887@gmail.com>
2019-02-03 19:20:35 +0000

    added current version of app

---
+++ app/src/main/java/com/example/a40203/tomtommapexample/MainActivity.java
@@ -0,0 +1,401 @@
+package com.example.a40203.tomtommapexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.Api;
+import com.google.common.base.Optional;
+import com.tomtom.online.sdk.common.location.LatLng;
+import com.tomtom.online.sdk.map.BaseMarkerBalloon;
+import com.tomtom.online.sdk.map.Icon;
+import com.tomtom.online.sdk.map.MapFragment;
+import com.tomtom.online.sdk.map.Marker;
+import com.tomtom.online.sdk.map.MarkerBuilder;
+import com.tomtom.online.sdk.map.OnMapReadyCallback;
+import com.tomtom.online.sdk.map.Route;
+import com.tomtom.online.sdk.map.RouteBuilder;
+import com.tomtom.online.sdk.map.SingleLayoutBalloonViewAdapter;
+import com.tomtom.online.sdk.map.TomtomMap;
+import com.tomtom.online.sdk.map.TomtomMapCallback;
+import com.tomtom.online.sdk.routing.OnlineRoutingApi;
+import com.tomtom.online.sdk.routing.RoutingApi;
+import com.tomtom.online.sdk.routing.data.FullRoute;
+import com.tomtom.online.sdk.routing.data.RouteQuery;
+import com.tomtom.online.sdk.routing.data.RouteQueryBuilder;
+import com.tomtom.online.sdk.routing.data.RouteResult;
+import com.tomtom.online.sdk.routing.data.RouteType;
+import com.tomtom.online.sdk.search.OnlineSearchApi;
+import com.tomtom.online.sdk.search.SearchApi;
+import com.tomtom.online.sdk.search.data.alongroute.AlongRouteSearchQueryBuilder;
+import com.tomtom.online.sdk.search.data.alongroute.AlongRouteSearchResponse;
+import com.tomtom.online.sdk.search.data.alongroute.AlongRouteSearchResult;
+import com.tomtom.online.sdk.search.data.reversegeocoder.ReverseGeocoderSearchQueryBuilder;
+import com.tomtom.online.sdk.search.data.reversegeocoder.ReverseGeocoderSearchResponse;
+import com.tomtom.online.sdk.traffic.OnlineTrafficApi;
+import com.tomtom.online.sdk.traffic.TrafficApi;
+
+import java.util.List;
+import java.util.Locale;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, TomtomMapCallback.OnMapLongClickListener, TextToSpeech.OnInitListener {
+
+    private TomtomMap tomtomMap;
+    private RoutingApi routingApi;
+    private SearchApi searchApi;
+    private TrafficApi trafficApi;
+    private Route route;
+    private LatLng departurePosition;
+    private LatLng destinationPosition;
+    private LatLng wayPointPosition;
+    private Icon departureIcon;
+    private Icon destinationIcon;
+    private ImageButton btnSearch;
+    private EditText editTextPois;
+    //text to speech
+    TextToSpeech mTTS = null;
+    private final int ACT_CHECK_TTS_DATA = 1000;
+    private final int REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE = 2000;
+    private int permissionCount = 0;
+    private String mAudioFilename = "";
+    private final String mUtteranceID = "totts";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initTomTomServices();
+        initUIViews();
+        setupUIViewListeners();
+
+        // Check to see if we have TTS voice data
+        Intent ttsIntent = new Intent();
+        ttsIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(ttsIntent, ACT_CHECK_TTS_DATA);
+    }
+
+    @Override
+    public void onMapReady(@NonNull final TomtomMap tomtomMap) {
+        this.tomtomMap = tomtomMap;
+        this.tomtomMap.setMyLocationEnabled(true);
+        this.tomtomMap.addOnMapLongClickListener(this);
+        this.tomtomMap.getMarkerSettings().setMarkersClustering(true);
+        this.tomtomMap.getMarkerSettings().setMarkerBalloonViewAdapter(createCustomViewAdapter());
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        if(isDeparturePositionSet() && isDestinationPositionSet()){
+            clearMap();
+        } else {
+            handleLongClick(latLng);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        this.tomtomMap.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    private void initTomTomServices() {
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getAsyncMap(this);
+        searchApi = OnlineSearchApi.create(this);
+        routingApi = OnlineRoutingApi.create(this);
+        trafficApi = OnlineTrafficApi.create(this);
+    }
+
+    private void initUIViews() {
+        departureIcon = Icon.Factory.fromResources(MainActivity.this, R.drawable.ic_map_route_departure);
+        destinationIcon = Icon.Factory.fromResources(MainActivity.this, R.drawable.ic_map_route_destination);
+        btnSearch = findViewById(R.id.btn_main_poisearch);
+        editTextPois = findViewById(R.id.edittext_main_poisearch);
+    }
+
+    private void setupUIViewListeners() {
+        View.OnClickListener searchButtonListener = getSearchButtonListener();
+        btnSearch.setOnClickListener(searchButtonListener);
+    }
+
+    private void clearMap(){
+        tomtomMap.clear();
+        departurePosition = null;
+        destinationPosition = null;
+        route = null;
+    }
+
+    private void handleLongClick(@NonNull LatLng latLng){
+        searchApi.reverseGeocoding(new ReverseGeocoderSearchQueryBuilder(latLng.getLatitude(), latLng.getLongitude()))
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new DisposableSingleObserver<ReverseGeocoderSearchResponse>() {
+            @Override
+            public void onSuccess(ReverseGeocoderSearchResponse response){
+                processResponse(response);
+            }
+
+            @Override
+            public void onError(Throwable e){
+                handleApiError(e);
+            }
+
+            private void processResponse(ReverseGeocoderSearchResponse response){
+                if(response.hasResults()){
+                    processFirstResult(response.getAddresses().get(0).getPosition());
+                }
+                else{
+                    Toast.makeText(MainActivity.this, getString(R.string.geocode_no_results), Toast.LENGTH_SHORT).show();
+                }
+            }
+            private void processFirstResult(LatLng geocodedPosition){
+                if(!isDeparturePositionSet()){
+                    setAndDisplayDeparturePosition(geocodedPosition);
+                } else{
+                    destinationPosition = geocodedPosition;
+                    tomtomMap.removeMarkers();
+                    drawRoute(departurePosition, destinationPosition);
+                }
+            }
+
+            private void setAndDisplayDeparturePosition(LatLng geocodedPosition){
+                departurePosition = geocodedPosition;
+                createMarkerIfNotPresent(departurePosition, departureIcon);
+            }
+        });
+    }
+
+    private boolean isDestinationPositionSet(){
+        return destinationPosition != null;
+    }
+
+    private boolean isDeparturePositionSet(){
+        return departurePosition != null;
+    }
+
+    private void handleApiError(Throwable e){
+        Toast.makeText(MainActivity.this, getString(R.string.api_response_error, e.getLocalizedMessage()), Toast.LENGTH_LONG).show();
+    }
+
+    private RouteQuery createRouteQuery(LatLng start, LatLng stop, LatLng[] wayPoints){
+        return (wayPoints != null) ?
+                new RouteQueryBuilder(start, stop).withWayPoints(wayPoints).withRouteType(RouteType.FASTEST) :
+                new RouteQueryBuilder(start, stop).withRouteType(RouteType.FASTEST);
+    }
+
+    private void drawRoute(LatLng start, LatLng stop){
+        wayPointPosition = null;
+        drawRouteWithWayPoints(start, stop, null);
+    }
+
+    private void drawRouteWithWayPoints(LatLng start, LatLng stop, LatLng[] wayPoints){
+        RouteQuery routeQuery = createRouteQuery(start, stop, wayPoints);
+        routingApi.planRoute(routeQuery)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableSingleObserver<RouteResult>() {
+                    @Override
+                    public void onSuccess(RouteResult routeResult){
+                        displayRoutes(routeResult.getRoutes());
+                        tomtomMap.displayRoutesOverview();
+                    }
+
+                    private void displayRoutes(List<FullRoute> routes){
+                        for(FullRoute fullRoute: routes){
+                            route = tomtomMap.addRoute(new RouteBuilder(
+                                    fullRoute.getCoordinates()).startIcon(departureIcon).endIcon(destinationIcon).isActive(true));
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e){
+                        handleApiError(e);
+                        clearMap();
+                    }
+                });
+    }
+    private void createMarkerIfNotPresent(LatLng position, Icon icon){
+        com.google.common.base.Optional<Marker> optionalMarker = tomtomMap.findMarkerByPosition(position);
+        if(!optionalMarker.isPresent()){
+            tomtomMap.addMarker(new MarkerBuilder(position).icon(icon));
+        }
+    }
+
+    @NonNull
+    private View.OnClickListener getSearchButtonListener(){
+        return new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                handleSearchClick(v);
+            }
+
+            private void handleSearchClick(View v){
+                if(isRouteSet()){
+                    Optional<CharSequence> description = Optional.fromNullable(v.getContentDescription());
+                    if(description.isPresent()){
+                        editTextPois.setText(description.get());
+
+                        v.setSelected(true);
+                    }
+                    if(isWayPointPositionSet()){
+                        tomtomMap.clear();
+                        drawRoute(departurePosition, destinationPosition);
+                    }
+                    String textToSearch = editTextPois.getText().toString();
+                    if(!textToSearch.isEmpty()){
+                        tomtomMap.removeMarkers();
+                        searchAlongTheRoute(route, textToSearch);
+                    }
+                }
+            }
+
+            private boolean isRouteSet(){
+                return route != null;
+            }
+
+            private boolean isWayPointPositionSet(){
+                return wayPointPosition != null;
+            }
+            private void searchAlongTheRoute(Route route, final String textToSearch){
+                final Integer MAX_DETOUR_TIME = 1000;
+                final Integer QUERY_LIMIT = 10;
+
+                searchApi.alongRouteSearch(new AlongRouteSearchQueryBuilder(textToSearch, route.getCoordinates(), MAX_DETOUR_TIME).withLimit(QUERY_LIMIT))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableSingleObserver<AlongRouteSearchResponse>() {
+                    @Override
+                    public void onSuccess(AlongRouteSearchResponse response){
+                        displaySearchResults(response.getResults());
+                    }
+
+                    private void displaySearchResults(List<AlongRouteSearchResult> results){
+                        if(!results.isEmpty()){
+                            for(AlongRouteSearchResult result:results){
+                                createAndDisplayCustomMarker(result.getPosition(), result);
+                            }
+                            tomtomMap.zoomToAllMarkers();
+                        } else{
+                            Toast.makeText(MainActivity.this, String.format(getString(R.string.no_search_results), textToSearch), Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    private void createAndDisplayCustomMarker(LatLng position, AlongRouteSearchResult result){
+                        String address = result.getAddress().getFreeformAddress();
+                        String poiName = result.getPoi().getName();
+
+                        BaseMarkerBalloon markerBalloonData = new BaseMarkerBalloon();
+                        markerBalloonData.addProperty(getString(R.string.poi_name_key), poiName);
+                        markerBalloonData.addProperty(getString(R.string.address_key), address);
+
+                        MarkerBuilder markerBuilder = new MarkerBuilder(position)
+                                .markerBalloon(markerBalloonData)
+                                .shouldCluster(true);
+                        tomtomMap.addMarker(markerBuilder);
+                    }
+
+                    @Override
+                    public void onError(Throwable e){
+                        handleApiError(e);
+                    }
+                });
+            }
+        };
+    }
+
+    private SingleLayoutBalloonViewAdapter createCustomViewAdapter(){
+        return new SingleLayoutBalloonViewAdapter(R.layout.marker_custom_ballon){
+            @Override
+            public void onBindView(View view, final Marker marker, BaseMarkerBalloon baseMarkerBalloon){
+                Button btnAddWayPoint = view.findViewById(R.id.btn_balloon_waypoint);
+                final TextView textViewPoiName = view.findViewById(R.id.textview_balloon_poiname);
+                TextView textViewPoiAddress = view.findViewById(R.id.textview_balloon_poiaddress);
+                textViewPoiName.setText(baseMarkerBalloon.getStringProperty(getApplicationContext().getString(R.string.poi_name_key)));
+                textViewPoiAddress.setText(baseMarkerBalloon.getStringProperty(getApplicationContext().getString(R.string.address_key)));
+                btnAddWayPoint.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        setWayPoint(marker);
+                        saySomething(textViewPoiName.getText().toString().trim(), 1);
+                    }
+                    private void setWayPoint(Marker marker){
+                        wayPointPosition = marker.getPosition();
+
+                        tomtomMap.clearRoute();
+                        drawRouteWithWayPoints(departurePosition, destinationPosition, new LatLng[] {wayPointPosition});
+                        marker.deselect();
+                    }
+                });
+            }
+        };
+    }
+
+
+    //text to speech
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            if (mTTS != null) {
+                int result = mTTS.setLanguage(Locale.US);
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Toast.makeText(this, "TTS language is not supported", Toast.LENGTH_LONG).show();
+                } else {
+                    saySomething("TTS is ready", 0);
+                }
+            }
+        } else {
+            Toast.makeText(this, "TTS initialization failed", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void saySomething(String text, int qmode) {
+        if (qmode == 1)
+            mTTS.speak(text, TextToSpeech.QUEUE_ADD, null);
+        else
+            mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == ACT_CHECK_TTS_DATA) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                // data exists, so we instantiate the TTS engine
+                mTTS = new TextToSpeech(this, this);
+            } else {
+                // data is missing, so we start the TTS installation process
+                Intent installIntent = new Intent();
+                installIntent.setAction(
+                        TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installIntent);
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mTTS != null) {
+            mTTS.stop();
+            mTTS.shutdown();
+        }
+
+        super.onDestroy();
+    }
+}
